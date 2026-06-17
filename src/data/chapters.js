3cfb988b035fb34897b65d5315eeb194f369b950
Bjorn <alex.stgelais@pf-cjp.com>
2026-06-17 11:13:06 -0400

    Point corrupted chapters to v1.0.1 release assets

--- src/data/chapters.js
+++ src/data/chapters.js
@@ -1,9 +1,12 @@
-const define = (id, era, title, file) => ({
+const V1 = 'https://github.com/cjpalex/VoiceOfChaos/releases/download/v1.0.0';
+const V2 = 'https://github.com/cjpalex/VoiceOfChaos/releases/download/v1.0.1';
+
+const define = (id, era, title, file, tag = V1) => ({
   id,
   era,
   title,
   file,
-  audio: `/audio/${file}`,
+  audio: `${tag}/${file}`,
   artwork: `/artwork/${file.replace('.mp3', '.jpg')}`,
 });
 
@@ -15,22 +18,22 @@ export const chapters = [
 
   // Rise of the Emperor
   define(4,  "Rise of the Emperor", "The Emperor Revealed",       "04-emperor-revealed.mp3"),
-  define(5,  "Rise of the Emperor", "The Unification Wars",       "05-unification-wars.mp3"),
+  define(5,  "Rise of the Emperor", "The Unification Wars",       "05-unification-wars.mp3",  V2),
 
   // Great Crusade
-  define(6,  "Great Crusade",       "The Primarchs: Origins",     "06-primarchs-origins.mp3"),
-  define(7,  "Great Crusade",       "The Legiones Astartes",      "07-legiones-astartes.mp3"),
-  define(8,  "Great Crusade",       "The Great Crusade",          "08-great-crusade.mp3"),
-  define(9,  "Great Crusade",       "Horus: The Beloved Son",     "09-horus-beloved-son.mp3"),
+  define(6,  "Great Crusade",       "The Primarchs: Origins",     "06-primarchs-origins.mp3", V2),
+  define(7,  "Great Crusade",       "The Legiones Astartes",      "07-legiones-astartes.mp3", V2),
+  define(8,  "Great Crusade",       "The Great Crusade",          "08-great-crusade.mp3",     V2),
+  define(9,  "Great Crusade",       "Horus: The Beloved Son",     "09-horus-beloved-son.mp3", V2),
 
   // Horus Heresy
-  define(10, "Horus Heresy",        "The Seeds of Heresy",        "10-seeds-of-heresy.mp3"),
-  define(11, "Horus Heresy",        "The Dropsite Massacre",      "11-dropsite-massacre.mp3"),
-  define(12, "Horus Heresy",        "The Siege of Terra",         "12-siege-of-terra.mp3"),
-  define(13, "Horus Heresy",        "The Death of Horus",         "13-death-of-horus.mp3"),
+  define(10, "Horus Heresy",        "The Seeds of Heresy",        "10-seeds-of-heresy.mp3",   V2),
+  define(11, "Horus Heresy",        "The Dropsite Massacre",      "11-dropsite-massacre.mp3", V2),
+  define(12, "Horus Heresy",        "The Siege of Terra",         "12-siege-of-terra.mp3",    V2),
+  define(13, "Horus Heresy",        "The Death of Horus",         "13-death-of-horus.mp3",    V2),
 
   // Age of Imperium
-  define(14, "Age of Imperium",     "The Golden Throne",          "14-golden-throne.mp3"),
+  define(14, "Age of Imperium",     "The Golden Throne",          "14-golden-throne.mp3",     V2),
   define(15, "Age of Imperium",     "The Holy Inquisition",       "15-holy-inquisition.mp3"),
 
   // 41st Millennium
